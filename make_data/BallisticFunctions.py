8a75dfffa444494c9011498c765667a4f4dc628a
spaceeye <superspaceeye@gmail.com>
2023-08-17 21:19:17 +0300

    fixes

---
+++ make_data/BallisticFunctions.py
@@ -0,0 +1,16 @@
+try:
+    from CannonBallisticFunctions import make_dataset
+except:
+    from fallback_py_funcs.py_make_dataset import make_dataset
+
+_make_dataset = make_dataset
+def make_dataset(charges, length, max_height_above=256, max_height_below=256,
+                 num_threads=16, verbose=True, max_steps=100000, max_length=600,
+                 step=1, impossible_cutoff=50, delta_t_max_overshoot=1,
+                 amin=-30, amax=60, gravity=0.05, num_iterations=5, num_elements=20,
+                 check_impossible=True):
+    return _make_dataset(charges, length, max_height_above, max_height_below,
+                         num_threads, verbose, max_steps,
+                         max_length, step, impossible_cutoff,
+                         delta_t_max_overshoot,
+                         amin, amax, gravity, num_iterations, num_elements, check_impossible)
--- make_data/make_dataset.py
+++ make_data/make_dataset.py
@@ -2,7 +2,7 @@ from BallisticFunctions import make_dataset
 import pickle
 
 if __name__ == "__main__":
-    res_n = make_dataset(8, 32, 256, 256, 1, True, 100000, 200, 1, 50, 1, check_impossible=False)
+    res_n = make_dataset(8, 32, 256, 256, 16, True, 100000, 600, 1, 500, 1, check_impossible=True)
     with open(f"../data", mode="wb") as file:
         pickle.dump(res_n, file)
 
